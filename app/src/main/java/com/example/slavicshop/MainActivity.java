ffee2d9d505fa13d26aaa6d68846e525c488d5bf
SpellD <58571337+SpellD@users.noreply.github.com>
2022-11-11 22:13:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/slavicshop/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.slavicshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton MyImageButton;
+    ImageButton MyImageButton1;
+    ImageButton MyImageButton2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyImageButton = (ImageButton) findViewById(R.id.my_image_button);
+
+        MyImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public  void onClick(View v) {
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        MyImageButton1 = (ImageButton) findViewById(R.id.my_image_button1);
+
+        MyImageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public  void onClick(View v) {
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, MainActivity3.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        MyImageButton2 = (ImageButton) findViewById(R.id.my_image_button2);
+
+        MyImageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public  void onClick(View v) {
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, MainActivity4.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/slavicshop/MainActivity4.java
@@ -0,0 +1,33 @@
+package com.example.slavicshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity4 extends AppCompatActivity {
+    private Button button;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+        button = (Button) findViewById(R.id.back);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
+    }
+
+    private void openActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
